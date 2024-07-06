d22f3b312e108dce7fce52d92b40dd07ee89ac08
StevenJB72 <129792430+StevenJB72@users.noreply.github.com>
2024-07-06 22:36:19 +0100

    Loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
-
+import Loading from '.Loading';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Simulate a data fetching operation
+    setTimeout(() => {
+      setData({ message: 'Data fetched successfully!' });
+      setIsLoading(false);
+    }, 3000); // Example delay of 3 seconds
+  }, []);
+
   return (
     <div className="App">
       {isLoading ? <Loading /> : <h1>Loaded!</h1>}
