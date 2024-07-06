85050e6bc2a193a13c4af91af39d04eef72f2549
StevenJB72 <129792430+StevenJB72@users.noreply.github.com>
2024-07-06 22:54:31 +0100

    s

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Red Panda</p>
+        <p>Steven Baird </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
