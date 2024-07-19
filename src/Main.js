7e5781c198dded3b0abdb6c061a139d2fab8f854
StevenJB72 <129792430+StevenJB72@users.noreply.github.com>
2024-07-19 21:42:56 +0100

    temp

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Main.css';
+
+//https://github.com/StevenJB72
+//NOTES: add link to github, linkedin, and a CV
+//NOTES: add a list of projects
+//NOTES: add moving background with three.js 
+//NOTES: add a landing page with an animation on it
+//NOTES: make projects a list
+
+function Main() {
+  return (
+    <div className="App">
+      <img src="/25511.jpg" alt="Description of the image" />
+    </div>
+  );
+}
+
+export default Main;
