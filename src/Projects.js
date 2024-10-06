92e1e60f4282b4c5d5b04f290c79e72f674453bf
StevenJB72 <129792430+StevenJB72@users.noreply.github.com>
2024-10-06 11:35:56 +0100

    basic stuff added

---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Project.css';
+
+const Projects = () => {
+    return (
+        <div className="projects">
+            {/* Your content goes here */}
+        </div>
+    );
+};
+
+export default Projects;
