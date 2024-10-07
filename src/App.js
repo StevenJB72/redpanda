fa0c0eb5ae6dc50c1d4aaf13104134aabf4a8a93
StevenJB72 <129792430+StevenJB72@users.noreply.github.com>
2024-10-07 15:41:30 +0100

    Custom domain fixed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Projects from './Projects';
 import Contact from './Contact';
-import githubLogo from './github-mark/github-mark.png';
-import linkedinLogo from './LinkedIn-Logos/LI-In-Bug.png';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="header-left">
-          <h1>Steven J Baird</h1>
-          <h2>Software Engineer</h2>
-        </div>
-        
-        <nav className="header-right">
+        <nav className="header-left">
+          <img src="" alt="Logo Here!" />
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#projects">Projects</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
       </header>
       
       <div className="App-body">
         <section id="home">
-          {/* Home section content */}
+          <div className='main-home'>
+          <h1>Steven J Baird</h1>
+          <h2>Software Engineer</h2>
+          </div>
         </section>
         
         <section id="projects">
@@ -37,20 +32,11 @@ function App() {
         <section id="about">
           {/* About section content */}
         </section>
-
-        <section id="contact">
-          <Contact />
-        </section>
       </div>
 
-      <footer>
-  <a href="https://github.com/StevenJB72">
-    <img src={githubLogo} alt="Link to GitHub" />
-  </a>
-  <a href="https://www.linkedin.com/in/steven-baird-b226aa290/">
-    <img src={linkedinLogo} alt="Link to LinkedIn" />
-  </a>
-</footer>
+      <footer className="App-footer">
+        <Contact />
+      </footer>
     </div>
   );
 }
--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Contact.css';
+import githubLogo from './github-mark/github-mark.svg';
+import linkedinLogo from './LinkedIn-Logos/LinkedIn_icon.svg';
+
 
 const Contact = () => {
     return (
         <div className="contact">
-            {/* Your content here */}
+            <a href="https://github.com/StevenJB72" target="_blank" rel="noopener noreferrer">
+                <img src={githubLogo} alt="Link to GitHub" className="contact-icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/steven-baird-b226aa290/" target="_blank" rel="noopener noreferrer">
+                <img src={linkedinLogo} alt="Link to LinkedIn" className="contact-icon" />
+            </a>
         </div>
     );
 };
